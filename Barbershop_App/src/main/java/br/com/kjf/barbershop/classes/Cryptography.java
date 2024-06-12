a0de2020a7de4ba4aa00d331efef6ab783f949b8
Kalil J. Fakhouri <kjfakhouri@gmail.com>
2024-06-12 00:34:52 -0300

    Update Cryptography.java

--- Barbershop_App/src/main/java/br/com/kjf/barbershop/classes/Cryptography.java
+++ Barbershop_App/src/main/java/br/com/kjf/barbershop/classes/Cryptography.java
@@ -1,7 +1,33 @@
 package br.com.kjf.barbershop.classes;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
 public class Cryptography {
 
+	public static SecretKey generateKey() throws NoSuchAlgorithmException {
+		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+		keyGen.init(256);
+		return keyGen.generateKey();
+	}
+	
+	public static String encrypt(String data, SecretKey key) 
+			throws NoSuchAlgorithmException, NoSuchPaddingException, 
+			InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		Cipher cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		byte[] encryptedBytes = cipher.doFinal(data.getBytes());
+		return Base64.getEncoder().encodeToString(encryptedBytes);
+	}
+	
 	
 	
 }
