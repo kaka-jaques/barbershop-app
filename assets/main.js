93ad051165d382733832eb63a9726e6609cb4446
Kalil J. Fakhouri <kjfakhouri@gmail.com>
2024-06-13 15:46:21 -0300

    created login button, login page and login function

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,7 @@ var planSwiper = new Swiper(".planSwiper", {
     followFinger: true,
     slidesPerView: 1,
     spaceBetween: 50,
-    navigation:{
+    navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
@@ -13,8 +13,8 @@ var planSwiper = new Swiper(".planSwiper", {
     }
 })
 
-window.addEventListener('scroll', function(){
-    if(window.scrollY > 100){
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 100) {
         gsap.to("header", {
             duration: 0.7,
             height: 85,
@@ -24,7 +24,11 @@ window.addEventListener('scroll', function(){
             duration: 0.7,
             width: 80
         })
-    }else{
+        gsap.to(".fa-user", {
+            duration: 0.7,
+            top: 20
+        })
+    } else {
         gsap.to("header", {
             duration: 0.5,
             height: 200
@@ -33,5 +37,61 @@ window.addEventListener('scroll', function(){
             duration: 0.5,
             width: 200
         })
-    } 
+        gsap.to(".fa-user", {
+            duration: 0.5,
+            top: 210
+        })
+    }
 })
+
+function GotoLogin(event) {
+    event.preventDefault();
+    loadContent('/login.html');
+    history.pushState({ page: 'login' }, 'Login', '/login');
+};
+
+function loadContent(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('content').innerHTML = data;
+        })
+        .catch(error => console.error('Error load content:', error));
+}
+
+window.addEventListener('popstate', () => {
+    if (window.location.pathname === '/login') {
+        loadContent('/login.html');
+    }
+    else {
+        loadContent('/index.html');
+    }
+})
+
+function sendLogin() {
+    if (document.getElementById('login-email').value != '' && document.getElementById('login-password').value != '') {
+        document.getElementById('login-button').innerHTML = '<i class="fa-solid fa-circle-notch fa-spin"></i>';
+        document.getElementById('login-button').disabled = true;
+        document.getElementById('login-button').style.cursor = 'not-allowed';
+        document.getElementById('login-email').style.border = '1px solid rgb(63, 63, 63)';
+        document.getElementById('login-password').style.border = '1px solid rgb(63, 63, 63)';
+        login()
+    } else{
+        if(document.getElementById('login-email').value === ''){
+            document.getElementById('login-email').style.border = '1px solid red';
+        }
+        else{
+            document.getElementById('login-email').style.border = '1px solid rgb(63, 63, 63)';
+        }
+        if(document.getElementById('login-password').value === ''){
+            document.getElementById('login-password').style.border = '1px solid red';
+        }
+        else{
+            document.getElementById('login-password').style.border = '1px solid rgb(63, 63, 63)';
+        }
+    }
+}
+
+function login() {
+    
+}
