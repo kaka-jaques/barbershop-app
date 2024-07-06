7ea663aefb5da8d1ef38e682f6e1cabaa363e94b
Kalil J. Fakhouri <kjfakhouri@gmail.com>
2024-07-06 01:07:30 -0300

    update ionic mobile app template

--- Barbershop_Webapp/barbershop-ios/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'barbershop-ios',
-  webDir: 'www'
-};
-
-export default config;
