c3836838d521301dfc946df3698e89b7fac39f32
Kalil J. Fakhouri <kjfakhouri@gmail.com>
2024-07-05 21:27:23 -0300

    initial commit ionic project

---
+++ Barbershop_Webapp/barbershop-ios/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'barbershop-ios',
+  webDir: 'www'
+};
+
+export default config;
