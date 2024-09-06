2aeaed5508494932dd8041ba8d24f8414b42894b
Kalil J. Fakhouri <kjfakhouri@gmail.com>
2024-09-06 12:27:27 -0300

    UPDATE
    dash circle fixed

--- Barbershop_Webapp/barbershop-mobile-app/src/app/pages/dashboard/dashboard.page.ts
+++ Barbershop_Webapp/barbershop-mobile-app/src/app/pages/dashboard/dashboard.page.ts
@@ -7,7 +7,7 @@ import { ConfigService } from 'src/app/config.service';
 import { register, SwiperContainer } from 'swiper/element/bundle';
 import { Swiper } from 'swiper/types';
 
-register(); 
+register();
 
 @Component({
   selector: 'app-dashboard',
@@ -25,10 +25,12 @@ export class DashboardPage implements OnInit {
   toastMessage: string = '';
   isToastOpen: boolean = false;
   sliderPosition: number = 0;
+  profitGroupVisible: boolean = false;
+  billGroupVisible: boolean = false;
+  viewToggleLegend: boolean = false;
 
-  profitChart = 0
-  dueChart = 0
-  billChart = 0
+  allBars: any = [];
+  allBarsCharts: any = [];
 
   profitBar: any;
   dueBar: any;
@@ -38,6 +40,12 @@ export class DashboardPage implements OnInit {
   dueQt: number = 1000;
   billQt: number = 0;
   totalQt: number = 0;
+
+  servicesQt: number = 0;
+  qtServicesQueue: any = {};
+  totalServicesQueue: number = 0;
+
+  servicesBars: any;
 
   month: number = new Date().getMonth();
   year: number = new Date().getFullYear();
@@ -75,12 +83,34 @@ export class DashboardPage implements OnInit {
     'Dezembro'
   ]
   slidesName = [
-    'Financeiro',
+    'Finança',
     'Serviços',
     'Slide 3'
   ]
+  ionColors = [
+    'primary',
+    'secondary',
+    'tertiary',
+    'success',
+    'warning',
+    'danger',
+    'dark',
+    'medium',
+    'light'
+  ]
+  hexaColors = [
+    '#0054e9',
+    '#0163aa',
+    '#6030ff',
+    '#2dd55b',
+    '#ffc409',
+    '#c5000f',
+    '#2f2f2f',
+    '#5f5f5f',
+    '#f6f8fc'
+  ]
 
-  constructor(private bill: BillService, private book: BookService, private config:ConfigService) { }
+  constructor(private bill: BillService, private book: BookService, private config: ConfigService) { }
 
   ngOnInit() {
 
@@ -111,117 +141,50 @@ export class DashboardPage implements OnInit {
       svgStyle: null
     })
 
-    this.profitBar = profitBar;
-    this.dueBar = dueBar;
-    this.billBar = billBar;
+    this.allBars = [profitBar, dueBar, billBar];
 
     forkJoin([this.bill.getBills(this.month + 1, this.year), this.book.getPeriodBookings(new Date(this.year, this.month, 1).toISOString(), new Date(this.year, this.month + 1, 0).toISOString()), this.config.getServices()])
       .pipe()
       .subscribe((response: any) => {
+
         this.bills = response[0].body.map((bill: any) => ({
           ...bill,
           value: bill.value - bill.value * 2,
           bookingDate: new Date(bill.year, bill.month - 1, bill.day).getTime(),
           type: bill.bill_type.name
-        }))
+        }));
+
         this.books = response[1].body.map((book: any) => ({
           ...book,
           value: book.services.price,
           type: book.services.name
-        }))
+        }));
+
         this.services = response[2].body;
+        this.servicesQt = this.services.length;
 
-        this.bills.forEach((bill: any) => {
-          this.billQt += bill.value - bill.value * 2;
+        this.services.forEach((service: any) => {
+          this.qtServicesQueue[service.name] = 0;
         });
+
         this.books.forEach((book: any) => {
-          this.profitQt += book.value;
-        });
-
-        if (this.profitQt < this.dueQt) {
-          this.totalQt = this.billQt + this.profitQt
-          this.dueQt = 0;
-        } else if (this.profitQt > this.billQt) {
-          this.dueQt = this.profitQt - this.billQt;
-          if (this.dueQt >= 1000) {
-            this.dueQt = 1000
+          if (book.services && book.services.name) {
+            const serviceName = book.services.name;
+            
+            if (this.qtServicesQueue.hasOwnProperty(serviceName)) {
+              this.qtServicesQueue[serviceName] += 1;
+            } else {
+              this.qtServicesQueue[serviceName] = 1; 
+            }
+            this.totalServicesQueue += 1;
           }
-          this.totalQt = this.billQt + this.dueQt + this.profitQt;
+        });
+        for (let i = 0; i < this.servicesQt; i++) {
+          this.services[i] = {
+            ...this.services[i],
+            pos: i
+          }
         }
-
-        this.profitChart = this.profitQt / this.totalQt;
-        this.dueChart = this.dueQt / this.totalQt;
-        this.billChart = this.billQt / this.totalQt;
-
-        this.data = [...this.bills, ...this.books];
-        this.data.sort((a: any, b: any) => a.bookingDate - b.bookingDate);
-        this.loading = false;
-        this.animateBars(billBar, dueBar, profitBar);
-      })
-
-  }
-
-  animateBars(billBar: any, dueBar: any, profitBar: any) {
-    billBar.animate(this.billChart);
-    dueBar.animate(this.dueChart + this.billChart);
-    profitBar.animate(this.profitChart + this.dueChart + this.billChart);
-    this.changeLoading = false;
-  }
-
-  nextSlide(swiper:SwiperContainer){
-    swiper.swiper.slideNext();
-  }
-
-  backSlide(swiper:SwiperContainer){
-    swiper.swiper.slidePrev();
-  }
-
-  prevMonth(){
-    this.changeLoading = true;
-    this.month--;
-    if(this.month < 0){
-      this.month = 11;
-      this.year--;
-    }
-    this.getPeriodData();
-  }
-
-  nextMonth(){
-    this.changeLoading = true;
-    this.month++;
-    if(this.month > 11){
-      this.month = 0;
-      this.year++;
-    }
-    this.getPeriodData();
-  }
-
-  getPeriodData(){
-
-    this.billQt = 0;
-    this.profitQt = 0;
-    this.profitChart = 0;
-    this.dueChart = 0;
-    this.billChart = 0;
-    this.totalQt = 0;
-
-    const bars = [this.dueBar, this.billBar, this.profitBar];
-    const resetBars = bars.map((bar: any) => this.resetBarsProgress(bar));
-
-    forkJoin([this.bill.getBills(this.month + 1, this.year), this.book.getPeriodBookings(new Date(this.year, this.month, 1).toISOString(), new Date(this.year, this.month + 1, 0).toISOString()), ...resetBars])
-      .pipe()
-      .subscribe((response: any) => {
-        this.bills = response[0].body.map((bill: any) => ({
-          ...bill,
-          value: bill.value - bill.value * 2,
-          bookingDate: new Date(bill.year, bill.month - 1, bill.day).getTime(),
-          type: bill.bill_type.name
-        }))
-        this.books = response[1].body.map((book: any) => ({
-          ...book,
-          value: book.services.price,
-          type: book.services.name
-        }))
 
         this.bills.forEach((bill: any) => {
           this.billQt += bill.value - bill.value * 2;
@@ -242,26 +205,193 @@ export class DashboardPage implements OnInit {
         }
 
         if (this.totalQt > 0 && !isNaN(this.totalQt)) {
-          this.profitChart = this.profitQt / this.totalQt;
-          this.dueChart = this.dueQt / this.totalQt;
-          this.billChart = this.billQt / this.totalQt;
-      } else if(this.totalQt < 0) {
-        this.totalQt = this.totalQt * -1
-        this.profitChart = this.profitQt / this.totalQt;
-        this.dueChart = this.dueQt / this.totalQt;
-        this.billChart = this.billQt / this.totalQt;
-      }else{
-        this.totalQt = this.profitQt + this.dueQt + this.billQt
-        this.profitChart = this.profitQt / this.totalQt;
-        this.dueChart = this.dueQt / this.totalQt;
-        this.billChart = this.billQt / this.totalQt;
-      }
+          this.allBarsCharts[0] = 1
+          this.allBarsCharts[2] = this.billQt / this.totalQt;
+          this.allBarsCharts[1] = (this.dueQt / this.totalQt) + this.allBarsCharts[2];
+        } else if (this.totalQt < 0) {
+          this.totalQt = this.totalQt * -1
+          this.allBarsCharts[0] = 1
+          this.allBarsCharts[2] = this.billQt / this.totalQt;
+          this.allBarsCharts[1] = (this.dueQt / this.totalQt) + this.allBarsCharts[2];
+        } else {
+          this.totalQt = this.profitQt + this.dueQt + this.billQt
+          this.allBarsCharts[0] = 1
+          this.allBarsCharts[2] = this.billQt / this.totalQt;
+          this.allBarsCharts[1] = (this.dueQt / this.totalQt) + this.allBarsCharts[2];
+        }
 
         this.data = [...this.bills, ...this.books];
         this.data.sort((a: any, b: any) => a.bookingDate - b.bookingDate);
         this.loading = false;
+
+        setTimeout(() => {
+
+          let a = 1;
+
+          for (let i = 0; i < this.servicesQt; i++) {
+            var bar = new ProgressBar.Circle('#bar' + i, {
+              strokeWidth: 6,
+              easing: 'easeInOut',
+              duration: 1000,
+              color: this.hexaColors[i],
+              trailColor: 'transparent',
+              svgStyle: null
+            })
+            
+            if(i>0){
+              this.allBarsCharts[this.servicesQt+2-i] =(this.qtServicesQueue[this.services[i].name]/this.totalServicesQueue) + this.allBarsCharts[this.servicesQt+3-i];
+            }else{
+              this.allBarsCharts[this.servicesQt+2] = (this.qtServicesQueue[this.services[i].name]/this.totalServicesQueue)
+            }
+            this.allBars[i+3] = bar;
+          }
+          this.animateBars(this.allBars);
+          console.log(this.qtServicesQueue);
+          
+        }, 50)
+
+      })
+
+  }
+
+  animateBars(bars: any) {
+    for (let i = 0; i < bars.length; i++) {
+      if (this.sliderPosition == 0) {
+        if (i < 3) {
+          bars[i].animate(this.allBarsCharts[i]);
+        } else {
+          break;
+        }
+      } else if (this.sliderPosition == 1) {
+        if (i > 2) {
+          bars[i]?.animate(this.allBarsCharts[i]);
+        }
+      }
+    }
+    this.changeLoading = false;
+  }
+
+  nextSlide(swiper: SwiperContainer) {
+    swiper.swiper.slideNext();
+  }
+
+  backSlide(swiper: SwiperContainer) {
+    swiper.swiper.slidePrev();
+  }
+
+  prevMonth() {
+    this.changeLoading = true;
+    this.month--;
+    if (this.month < 0) {
+      this.month = 11;
+      this.year--;
+    }
+    this.getPeriodData();
+  }
+
+  nextMonth() {
+    this.changeLoading = true;
+    this.month++;
+    if (this.month > 11) {
+      this.month = 0;
+      this.year++;
+    }
+    this.getPeriodData();
+  }
+
+  getPeriodData() {
+
+    this.billQt = 0;
+    this.profitQt = 0;
+    this.allBarsCharts = []
+    this.totalQt = 0;
+    
+    Object.keys(this.qtServicesQueue).forEach((key: string) => {
+      this.qtServicesQueue[key] = 0;
+    });
+
+    forkJoin([this.bill.getBills(this.month + 1, this.year), this.book.getPeriodBookings(new Date(this.year, this.month, 1).toISOString(), new Date(this.year, this.month + 1, 0).toISOString()), this.resetBarsProgress(this.allBars)])
+      .pipe()
+      .subscribe((response: any) => {
+        this.bills = response[0].body.map((bill: any) => ({
+          ...bill,
+          value: bill.value - bill.value * 2,
+          bookingDate: new Date(bill.year, bill.month - 1, bill.day).getTime(),
+          type: bill.bill_type.name
+        }));
+
+        this.books = response[1].body.map((book: any) => ({
+          ...book,
+          value: book.services.price,
+          type: book.services.name
+        }));
+
+        this.books.forEach((book: any) => {
+          if (book.services && book.services.name) {
+            const serviceName = book.services.name;
+            
+            if (this.qtServicesQueue.hasOwnProperty(serviceName)) {
+              this.qtServicesQueue[serviceName] += 1;
+            } else {
+              this.qtServicesQueue[serviceName] = 1; 
+            }
+          } else {
+            console.error("Serviço não encontrado ou nome do serviço está undefined:", book);
+          }
+        }); 
+
+        this.bills.forEach((bill: any) => {
+          this.billQt += bill.value - bill.value * 2;
+        });
+        this.books.forEach((book: any) => {
+          this.profitQt += book.value;
+        });
+
+        if (this.profitQt < this.dueQt) {
+          this.totalQt = this.billQt + this.profitQt
+          this.dueQt = 0;
+        } else if (this.profitQt > this.billQt) {
+          this.dueQt = this.profitQt - this.billQt;
+          if (this.dueQt >= 1000) {
+            this.dueQt = 1000
+          }
+          this.totalQt = this.billQt + this.dueQt + this.profitQt;
+        }
+
+        if (this.totalQt > 0 && !isNaN(this.totalQt)) {
+          this.allBarsCharts[0] = 1
+          this.allBarsCharts[2] = this.billQt / this.totalQt;
+          this.allBarsCharts[1] = (this.dueQt / this.totalQt) + this.allBarsCharts[2];
+        } else if (this.totalQt < 0) {
+          this.totalQt = this.totalQt * -1
+          this.allBarsCharts[0] = 1
+          this.allBarsCharts[2] = this.billQt / this.totalQt;
+          this.allBarsCharts[1] = (this.dueQt / this.totalQt) + this.allBarsCharts[2];
+        } else {
+          this.totalQt = this.profitQt + this.dueQt + this.billQt
+          this.allBarsCharts[0] = 1
+          this.allBarsCharts[2] = this.billQt / this.totalQt;
+          this.allBarsCharts[1] = (this.dueQt / this.totalQt) + this.allBarsCharts[2];
+        }
+
+        this.data = [...this.bills, ...this.books];
+        this.data.sort((a: any, b: any) => a.bookingDate - b.bookingDate);
+        this.loading = false;
+
+        for (let i = 0; i < this.servicesQt; i++) {
+          
+          if(i>0){
+            this.allBarsCharts[this.servicesQt+2-i] =(this.qtServicesQueue[this.services[i].name]/this.totalServicesQueue) + this.allBarsCharts[this.servicesQt+3-i];
+          }else{
+            this.allBarsCharts[this.servicesQt+2] = (this.qtServicesQueue[this.services[i].name]/this.totalServicesQueue)
+          }
+        }
+
+        this.animateBars(this.allBars);
+        console.log(this.services);
+        console.log(this.qtServicesQueue);
+        console.log(this.allBarsCharts);
         
-        this.animateBars(this.billBar, this.dueBar, this.profitBar);
       }, (error) => {
         this.toastColor = 'danger';
         this.toastMessage = 'Ocorreu um erro ao carregar os dados. Tente novamente mais tarde.';
@@ -270,14 +400,19 @@ export class DashboardPage implements OnInit {
       })
   }
 
-  resetBarsProgress(bar:any){
+  resetBarsProgress(bars: any) {
     return new Promise((resolve) => {
-      bar.animate(0, () => {resolve(true)});
+      for(let bar of bars){
+        bar.animate(0);
+      }
+      resolve(true);
     })
   }
 
-  onSlideChange(swiper:SwiperContainer) {
+  onSlideChange(swiper: SwiperContainer) {
     this.sliderPosition = swiper.swiper.realIndex;
+    this.resetBarsProgress(this.allBars)
+    this.animateBars(this.allBars);
   }
 
 }
